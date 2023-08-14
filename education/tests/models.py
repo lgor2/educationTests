9a225335b6e2d3e3ec40f4bf9d9e0e699ebd7471
Ihor <lgor.2.lgor.2.lgor@gmail.com>
2023-08-14 13:47:32 +0300

    Add registration and login/logout

--- education/tests/models.py
+++ education/tests/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    pass
+
+
+class Teacher(models.Model):
+    pass
+
+
+class Test(models.Model):
+    title = models.CharField(max_length=500)
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=1000)
+
+
+class Answer(models.Model):
+    text = models.CharField(max_length=500)
--- education/tests/views.py
+++ education/tests/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import Group, User
+
+
+def get_group(req):
+    if req.POST['role'] == 'S':
+        group, created = Group.objects.get_or_create(name='student_group')
+        return group
+    elif req.POST['role'] == 'T':
+        group, created = Group.objects.get_or_create(name='teacher_group')
+        return group
+
 
 # Create your views here.
-
-
 def home(request):
-    return render(request, 'tests/home.html')
+    user = request.user
+    is_student = user.groups.filter(name='student_group').exists()
+    is_teacher = user.groups.filter(name='teacher_group').exists()
+
+    context = {
+        'user': user,
+        'is_student': is_student,
+        'is_teacher': is_teacher,
+    }
+
+    return render(request, 'tests/home.html', context=context)
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Add user to a group
+            group = get_group(request)
+            user.groups.add(group)
+
+            login(request, user)
+            return redirect('/home')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/sign_up.html', {"form": form})
