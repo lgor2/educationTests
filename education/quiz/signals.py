9b9b47c2b43a6721921bf666768ab004a7f0a502
Ihor <lgor.2.lgor.2.lgor@gmail.com>
2024-02-07 22:11:38 +0200

    Connected signals to the app

--- education/quiz/signals.py
+++ education/quiz/signals.py
@@ -1,17 +1,18 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from models import Question
+from .models import Question
 
 
 @receiver(post_save, sender=Question)
-def update_quiz_question_count(sender, instance):
+def update_quiz_question_count(sender, instance, **kwargs):
     quiz = instance.related_quiz
     quiz.question_count = Question.objects.filter(related_quiz=quiz).count()
+    print("Signals", quiz.question_count)
     quiz.save()
 
 
 @receiver(post_delete, sender=Question)
-def update_quiz_question_count_on_delete(sender, instance):
+def update_quiz_question_count_on_delete(sender, instance, **kwargs):
     quiz = instance.related_quiz
     quiz.question_count = Question.objects.filter(related_quiz=quiz).count()
     quiz.save()
